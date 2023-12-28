2a0144b75dd8484fe7731e53fe269eae4d392f61
Alejandro Greggio <agreggio@slingr.io>
2023-12-28 17:51:53 -0300

    #1 - Task: Migrate Infura endpoint to a package

--- scripts/api.js
+++ scripts/api.js
@@ -25,11 +25,6 @@ function handleRequestWithRetry(requestFn, options, callbackData, callbacks) {
         return requestFn(options, callbackData, callbacks);
     } catch (error) {
         sys.logs.info("[infura] Handling request..."+ JSON.stringify(error));
-        // TODO : If you use oauth uncomment this, otherwise delete this comment
-        /*
-        dependencies.oauth.functions.refreshToken('infura:refreshToken');
-        return requestFn(setAuthorization(options), callbackData, callbacks);
-        */
     }
 }
 
@@ -41,28 +36,6 @@ function createWrapperFunction(requestFn) {
 
 for (let key in httpDependency) {
     if (typeof httpDependency[key] === 'function') httpService[key] = createWrapperFunction(httpDependency[key]);
-}
-
-// TODO If use oauth you will need the following two functions, otherwise delete them
-
-/**
- * Retrieves the access token.
- *
- * @return {void} The access token refreshed on the storage.
- */
-exports.getAccessToken = function () {
-    sys.logs.info("[infura] Getting access token from oauth");
-    return dependencies.oauth.functions.connectUser('infura:userConnected');
-}
-
-/**
- * Removes the access token from the oauth.
- *
- * @return {void} The access token removed on the storage.
- */
-exports.removeAccessToken = function () {
-    sys.logs.info("[infura] Removing access token from oauth");
-    return dependencies.oauth.functions.disconnectUser('infura:disconnectUser');
 }
 
 /****************************************************
@@ -266,18 +239,9 @@ let stringType = Function.prototype.call.bind(Object.prototype.toString)
  Configurator
  ****************************************************/
 
-// TODO This is for the uncommon case that you need to execute something when the app is redeployed or in the first call
-// TODO Remove this variable if you don't need it
-
-let init = true;
-
-// TODO Refactor the Infura function to your package name
-
 let Infura = function (options) {
-    if (init) { methodOnInit(); init= false; } // TODO Remove this line if you don't use the init variable
     options = options || {};
     options= setApiUri(options);
-    options= setAuthorization(options);
     options= setRequestHeaders(options);
     return options;
 }
@@ -287,6 +251,7 @@ let Infura = function (options) {
  ****************************************************/
 
 function setApiUri(options) {
+    var API_URL = config.get("INFURA_API_BASE_URL");
     let url = options.path || "";
     options.url = API_URL + url;
     sys.logs.debug('[infura] Set url: ' + options.path + "->" + options.url);
@@ -295,46 +260,9 @@ function setApiUri(options) {
 
 function setRequestHeaders(options) {
     let headers = options.headers || {};
-    if (config.get("choice") === "apiKey") { // TODO: Set the authentication method, if needed or remove this if (Remove comments after set the url)
-        sys.logs.debug('[infura] Set header apikey');
-        headers = mergeJSON(headers, {"Authorization": "API-Key " + config.get("text")});
-    } 
     headers = mergeJSON(headers, {"Content-Type": "application/json"});
-
     options.headers = headers;
     return options;
-}
-
-function setAuthorization(options) { // TODO: Set the authorization method and verify prefix, if needed or remove this function (Remove comments after set the url)
-    sys.logs.debug('[infura] Setting header token oauth');
-    let authorization = options.authorization || {};
-    authorization = mergeJSON(authorization, {
-        type: "oauth2",
-        accessToken: sys.storage.get(config.get("oauth").id + ' - access_token', {decrypt:true}),
-        headerPrefix: "token"
-    });
-    options.authorization = authorization;
-    return options;
-}
-
-function methodOnInit(){
-    let refreshTokenResponse = httpService.post({
-        url: "https://example.com/",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: {"grant_type":"refresh_token","refresh_token" : config.get("refreshToken")},
-        authorization: {
-            type: "basic",
-            username: config.get("clientId"),
-            password: config.get("clientSecret")
-        }
-    });
-    sys.logs.debug('[infura] Refresh token response: ' + JSON.stringify(refreshTokenResponse));
-    // If you need to set a variable at application level, you can do it with _config.set (on redeploy its cleared)
-    _config.set("accessToken", refreshTokenResponse.access_token);
-    _config.set("refreshToken", refreshTokenResponse.refresh_token);
 }
 
 function mergeJSON (json1, json2) {
